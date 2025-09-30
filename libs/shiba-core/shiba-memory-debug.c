a2020d444921b470f91a9b04b9ca0b12b815a232
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-09-30 14:28:20 -0500

    implement: shiba mem debug realloc. changed debug remove position to be above it

--- libs/shiba-core/shiba-memory-debug.c
+++ libs/shiba-core/shiba-memory-debug.c
@@ -150,10 +150,6 @@ void* shiba_memory_debug_malloc(uint size, char* file, uint line) {
   return ptr;
 }
 
-void* shiba_memory_debug_realloc(void* ptr, uint size, char* file, uint line) {
-  // TODO:
-}
-
 boolean shiba_memory_debug_remove(void* buf) {
   for (int i = 0; i < shiba_alloc_line_count; i++) {
     for (int j = 0; j < shiba_alloc_lines[i].alloc_count; j++)
@@ -177,6 +173,72 @@ boolean shiba_memory_debug_remove(void* buf) {
     }
   }
   return FALSE;
+}
+
+void* shiba_memory_debug_realloc(void* ptr, uint size, char* file, uint line) {
+  void* ptr2; uint i, j, k, move;
+
+  if (!ptr)
+    return shiba_memory_debug_malloc(size, file, line);
+
+  if (shiba_alloc_mutex)
+    shiba_alloc_mutex_lock(shiba_alloc_mutex);
+
+  for (i = 0; i < shiba_alloc_line_count; i++) {
+    for (j = 0; j < shiba_alloc_lines[i].alloc_count; j++)
+    {
+      if (shiba_alloc_lines[i].allocs[j].buffer == ptr)
+        break;
+
+      if (j < shiba_alloc_lines[i].alloc_count)
+        break;
+    }
+    
+    if (i == shiba_alloc_line_count) {
+      fprintf(stderr, "MEMORY ERROR: Rellocation of pointer %p in %s line %u failed. Pointer was never allocated!\n", ptr, file, line);
+
+      for (i = 0; i < shiba_alloc_line_count; i++)
+      {
+        uint* buf = shiba_alloc_lines[i].allocs[j].buffer;
+        for (k = 0; k < shiba_alloc_lines[i].allocs[j].size; k++) {
+          if (&buf[k] == ptr)
+            printf("Trying to reallocate pointer of %u bytes (out of %u) into alloc made in %s on line %u!\n", k shiba_alloc_lines[i].allocs[j].size, shiba_alloc_lines[i].file, shiba_alloc_lines[i].line);
+        }
+      }
+    }
+    exit(0);
+  }
+
+  move = shiba_alloc_lines[i].allocs[j].size;
+
+  if (size < move)
+    move = size;
+
+  ptr2 = malloc(size + SHIBA_MEM_OVER_ALLOC);
+
+  if (!ptr2) {
+    fprintf(stderr, "MEMORY ERROR: Realloc returns NULL trying to alloc %u bytes at line %u in file %s!\n", size, line, file);
+
+    if (shiba_alloc_mutex)
+      shiba_alloc_mutex_unlock(shiba_alloc_mutex);
+
+    shiba_memory_debug_print(0);
+    exit(0);
+  }
+
+  for (i = 0; i < size + SHIBA_MEM_OVER_ALLOC; i++)
+      ((uint8* ) ptr2)[i] = SHIBA_MEM_MAGIC_NUM + 1;
+
+  memcpy(ptr2, ptr, move);
+
+  shiba_memory_debug_add(ptr2, size, file, line);
+  shiba_memory_debug_remove(ptr);
+  free(ptr);
+
+  if (shiba_alloc_mutex)
+  shiba_alloc_mutex_unlock(shiba_alloc_mutex);
+
+return ptr2;
 }
 
 void shiba_memory_debug_free(void* buffer) {
