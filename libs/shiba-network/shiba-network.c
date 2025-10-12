005967215cd89e24fdf14c8291c675ebdf0a5429
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-12 13:13:23 -0500

    add: linux valid ipv4 and port implementations. TODO: Windows implementation for ipv4 validation

--- libs/shiba-network/shiba-network.c
+++ libs/shiba-network/shiba-network.c
@@ -1,3 +1,25 @@
 #include "shiba-network.h"
 
 
+boolean is_valid_ipv4_address(const char* ip_addr) {
+  if (!ip_addr)
+    return 0;
+
+  #ifdef _WIN32
+  // win32 stuff here
+  return 0; // temporary. will fix when im on windows
+  #else
+  struct in_addr addr;
+  return inet_pton(AF_INET, ip_addr, &addr) == 1;
+  #endif
+}
+
+boolean is_valid_port(const int32 port) {
+  if (!port)
+    return 0;
+
+  if (port < 1 || port > 65535)
+    return 0;
+
+  return 1;
+}
