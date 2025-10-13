85f23d7539015be97ae5d6996318edeb4d87a888
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-12 21:20:32 -0500

    add: more tests for new functions

--- main.c
+++ main.c
@@ -11,4 +11,15 @@ int main(int argc, char *argv[])
 
   shiba_network_is_valid_ipv4_address(addr) ? printf("Valid IPv4 Address!\n") : printf("Invalid IPv4 Address!\n");
   shiba_network_is_valid_port(port) ? printf("Valid Port!\n") : printf("Invalid Port!\n");
+
+  boolean init = shiba_network_init();
+  init ? printf("Init succeeded!\n") : printf("Init failed!\n");
+
+  shiba_network_socket_t* sock = shiba_network_create_socket(AF_INET, SOCK_STREAM, 0);
+  sock ? printf("Socket init succeeded!\n") : printf("Socket init failed!\n");
+
+  shiba_network_destroy_socket(sock);
+  printf("Destroyed socket successfully!\n");
+
+  shiba_network_cleanup();
 }
