044c3e4cc72d82b604f1c2a796d8ad5f92f33ec3
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-12 21:27:29 -0500

    chore: add indentation after WIN32 check

--- libs/shiba-network/shiba-network.c
+++ libs/shiba-network/shiba-network.c
@@ -9,13 +9,13 @@ boolean shiba_network_is_valid_ipv4_address(const char* ip_addr) {
     return 0;
 
   #ifdef _WIN32
-  int result = inet_addr(ip_addr);
-  if (result == INADDR_NONE || result == INADDR_ANY)
-      return 0;
-  return 1;
+    int result = inet_addr(ip_addr);
+    if (result == INADDR_NONE || result == INADDR_ANY)
+        return 0;
+    return 1;
   #else
-  struct in_addr addr;
-  return inet_pton(AF_INET, ip_addr, &addr) == 1;
+    struct in_addr addr;
+    return inet_pton(AF_INET, ip_addr, &addr) == 1;
   #endif
 }
 
