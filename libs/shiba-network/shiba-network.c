176961fbaf838391842b629fa18a642a4ade7306
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-12 20:22:46 -0500

    add: windows IPv4 validation implementation

--- libs/shiba-network/shiba-network.c
+++ libs/shiba-network/shiba-network.c
@@ -6,8 +6,10 @@ boolean shiba_network_is_valid_ipv4_address(const char* ip_addr) {
     return 0;
 
   #ifdef _WIN32
-  // win32 stuff here
-  return 0; // temporary. will fix when im on windows
+  int result = inet_addr(ip_addr);
+  if (result == INADDR_NONE || result == INADDR_ANY)
+      return 0;
+  return 1;
   #else
   struct in_addr addr;
   return inet_pton(AF_INET, ip_addr, &addr) == 1;
