53e85d5c959a01e73ef58f05f49111497572f437
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-18 16:37:54 -0500

    fix: indentation

--- libs/shiba-network/shiba-network.c
+++ libs/shiba-network/shiba-network.c
@@ -8,15 +8,15 @@ boolean shiba_network_is_valid_ipv4_address(const char* ip_addr) {
   if (!ip_addr)
     return 0;
 
-  #ifdef _WIN32
-    int result = inet_addr(ip_addr);
-    if (result == INADDR_NONE || result == INADDR_ANY)
-        return 0;
-    return 1;
-  #else
-    struct in_addr addr;
-    return inet_pton(AF_INET, ip_addr, &addr) == 1;
-  #endif
+#ifdef _WIN32
+  int result = inet_addr(ip_addr);
+  if (result == INADDR_NONE || result == INADDR_ANY)
+      return 0;
+  return 1;
+#else
+  struct in_addr addr;
+  return inet_pton(AF_INET, ip_addr, &addr) == 1;
+#endif
 }
 
 boolean shiba_network_is_valid_port(const int32 port) {
