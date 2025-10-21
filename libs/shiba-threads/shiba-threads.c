186d0069c86dbeeba0857af4503165711e4d691c
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-21 14:56:53 -0500

    add: declarations to shiba threads.c (they are NOT implemented)

--- libs/shiba-threads/shiba-threads.c
+++ libs/shiba-threads/shiba-threads.c
@@ -1,3 +1,29 @@
 #include "./shiba-threads.h"
 
+uint32 shiba_threads_thread_create(shiba_threads_thread_t* thread, void* (*function) (void*), void* arg) {
+  // TODO:
+}
 
+uint32 shiba_threads_thread_join(shiba_threads_thread_t* thread, void** retval) {
+  // TODO:
+}
+
+uint32 shiba_threads_thread_destroy(shiba_threads_thread_t* thread) {
+  // TODO:
+}
+
+uint32 shiba_threads_mutex_init(shiba_threads_mutex_t* mutex) {
+  // TODO:
+}
+
+uint32 shiba_threads_mutex_lock(shiba_threads_mutex_t* mutex) {
+  // TODO:
+}
+
+uint32 shiba_threads_mutex_unlock(shiba_threads_mutex_t* mutex) {
+  // TODO:
+}
+
+uint32 shiba_threads_mutex_destroy(shiba_threads_mutex_t* mutex) {
+  // TODO:
+}
