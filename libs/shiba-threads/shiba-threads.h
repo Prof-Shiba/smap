81ae87eb70a46c651fab4593a23d69a9afa420fa
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-21 20:15:51 -0500

    move: result uinptr to win_convert_t. move freeing logic to thread join. add a result context for storing ret vals.

--- libs/shiba-threads/shiba-threads.h
+++ libs/shiba-threads/shiba-threads.h
@@ -9,9 +9,26 @@
   #include <pthread.h>
 #endif
 
+#if defined _WIN32
+  typedef struct shiba_threads_win_convert_t {
+    void* arg;
+    void* (*function) (void*);
+    uintptr_t result;
+  }shiba_threads_win_convert_t;
+
+  static DWORD WINAPI shiba_threads_windows_thread_create(LPVOID param) {
+    shiba_threads_win_convert_t* win = (shiba_threads_win_convert_t*)param;
+    void* res = win->function(win->arg);
+    win->result = (uintptr_t)res;
+
+    return 0;
+  }
+#endif
+
 typedef struct shiba_threads_thread_t {
 #if defined _WIN32
   HANDLE thread;
+  shiba_threads_win_convert_t* result_context;
 #else
   pthread_t thread;
 #endif
@@ -25,24 +42,10 @@ typedef struct shiba_threads_mutex_t {
 #endif
 } shiba_threads_mutex_t;
 
-// NOTE: windows wants DWORD WINAPI ThreadFunc(LPVOID lpParam) where DWORD = int32 and LPVOID is just a void*
-// WINAPI should just be like __stdcall or something
-
-/*
-DWORD WINAPI function(LPVOID lpParam) {
-  printf("some text\n");
-  return 0;
-}
-
-this needs to be converted^
-*/
-
 // Creates a thread, returns 0 on success
 uint32 shiba_threads_thread_create(shiba_threads_thread_t* handle, void* (*function) (void*), void* arg);
-// Join thread, returns 0 on success
+// Join thread, returns 0 on success, also cleansup
 uint32 shiba_threads_thread_join(shiba_threads_thread_t* handle, void** retval);
-// Destroy a thread
-void shiba_threads_thread_destroy(shiba_threads_thread_t* handle);
 
 // init a mutex, returns 0 on success
 uint32 shiba_threads_mutex_init(shiba_threads_mutex_t* handle);
@@ -52,22 +55,3 @@ uint32 shiba_threads_mutex_lock(shiba_threads_mutex_t* handle);
 uint32 shiba_threads_mutex_unlock(shiba_threads_mutex_t* handle);
 // destroy a mutex, returns 0 on success. shouldn't fail unless you passed an invalid mutex
 uint32 shiba_threads_mutex_destroy(shiba_threads_mutex_t* handle);
-
-// Read this: https://web.archive.org/web/20121023005749/http://www.cs.rpi.edu/academics/courses/netprog/WindowsThreads.html 
-
-/*
-windows equivalent to pthread_create:
-
-HANDLE CreateThread(
-  LPSECURITY_ATTRIBUTES lpThreadAttributes,  // pointer to security attributes - SET TO NULL
-  DWORD dwStackSize,                         // initial thread stack size - USE 0
-  LPTHREAD_START_ROUTINE lpStartAddress,     // pointer to thread function
-  LPVOID lpParameter,                        // argument for new thread
-  DWORD dwCreationFlags,                     // creation flags - USE 0
-  LPDWORD lpThreadId                         // pointer to receive thread ID - NULL
-);
-
-example:
-HANDLE CreateThread(NULL, 0, &func_here, &arg_to_func, 0, &ptr_to_thread_id); // returns a HANDLE if success, NULL if fail
-*/
-
