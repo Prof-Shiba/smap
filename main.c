52c2157c4160f7e874a84031906006d1d9931848
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-21 20:14:05 -0500

    updated: tests for testing multithreading library

--- main.c
+++ main.c
@@ -1,25 +1,54 @@
 #include "./libs/shiba-core/shiba.h"
 #include "./libs/shiba-network/shiba-network.h"
+#include "./libs/shiba-threads/shiba-threads.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+#define ARRAY_SIZE 20
+shiba_threads_thread_t threads[ARRAY_SIZE];
+shiba_threads_mutex_t mutex;
+int j = 0;
+
+void* function(void* arg) {
+  shiba_threads_mutex_lock(&mutex);
+  for (int i = 0; i < 100000; i++) {
+    j++;
+  }
+  shiba_threads_mutex_unlock(&mutex);
+}
+
+int main(int argc, char* argv[])
 {
   printf("smap starting...\n\n");
 
-  char* addr = "110.128.59.255";
-  int32 port = 65535;
+  if (shiba_threads_mutex_init(&mutex) != 0) {
+      fprintf(stderr, "Failed to create mutex!\n");
+      return 1;
+    }
 
-  shiba_network_is_valid_ipv4_address(addr) ? printf("Valid IPv4 Address!\n") : printf("Invalid IPv4 Address!\n");
-  shiba_network_is_valid_port(port) ? printf("Valid Port!\n") : printf("Invalid Port!\n");
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    if (shiba_threads_thread_create(threads + i, &function, NULL) != 0) {
+      fprintf(stderr, "Failed to create thread number: %d!\n", i);
+      return 1;
+    }
 
-  boolean init = shiba_network_init();
-  init ? printf("Init succeeded!\n") : printf("Init failed!\n");
+    printf("Thread %d has started!\n", i);
+  }
+  printf("\n");
 
-  shiba_network_socket_t* sock = shiba_network_create_socket(AF_INET, SOCK_STREAM, 0);
-  sock ? printf("Socket init succeeded!\n") : printf("Socket init failed!\n");
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    if (shiba_threads_thread_join(threads + i, NULL) != 0) {
+      fprintf(stderr, "Failed to join thread number: %d!\n", i);
+      return 1;
+    }
+    printf("Thread %d has been joined!\n", i);
+  }
+  printf("\n");
 
-  shiba_network_destroy_socket(sock);
-  printf("Destroyed socket successfully!\n");
+  if (shiba_threads_mutex_destroy(&mutex) != 0) {
+    fprintf(stderr, "Failed to destroy mutex!\n");
+    return 1;
+  }
 
-  shiba_network_cleanup();
+  printf("Value: %d\n", j);
+  return 0;
 }
