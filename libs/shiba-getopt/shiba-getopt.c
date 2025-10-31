cc180ad4c3bcf3b44bdd2a64275e3e6da0e7a6ad
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-31 15:30:31 -0500

    add: implementations for getopt_reset and getopt_long and long_only. TODO note for internal

--- libs/shiba-getopt/shiba-getopt.c
+++ libs/shiba-getopt/shiba-getopt.c
@@ -1,3 +1,30 @@
 #include "./shiba-getopt.h"
 
-// TODO:
+int opt_index = 1, opt_err = 1, opt_opt = 1;
+char* opt_arg = 0;
+
+int getopt_reset(void) {
+  opt_index = 1;
+  opt_err = 1;
+  opt_opt = 0;
+  opt_arg = 0;
+  return 0;
+}
+
+extern int _getopt_internal(int argc, char* argv[], const char* short_opts,
+            const struct option* long_opts, int* long_index, int long_only)
+{
+  // TODO:
+}
+
+int getopt_long(int argc, char* argv[], const char* short_opts,
+            const struct option* long_opts, int* long_index)
+{
+  return _getopt_internal(argc, argv, short_opts, long_opts, long_index, 0);
+}
+
+int getopt_long_only(int argc, char* argv[], const char* short_opts,
+            const struct option* long_opts, int* long_index)
+{
+  return _getopt_internal(argc, argv, short_opts, long_opts, long_index, 1);
+}
