a4b9a9e297e2e788f99f74dac6184ba3d9d8735c
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-10-31 17:04:32 -0500

    implement: arg-parser logic using getopt_long_only and switch statement

--- arg-parsing/arg-parser.c
+++ arg-parsing/arg-parser.c
@@ -1,8 +1,6 @@
 #include "./arg-parser.h"
 #include "../libs/shiba-core/shiba.h"
-
-// TODO: We will start by just making sure we can print the usage function
-// with '-h' or '--help' and expand from there
+#include "../libs/shiba-getopt/shiba-getopt.h"
 
 int parse_args(int argc, char *argv[]) {
   if (argc <= 1) {
@@ -10,22 +8,55 @@ int parse_args(int argc, char *argv[]) {
     exit(1);
   }
 
-  // custom getopt_long_only function that works on both windows and linux
-  // looks like while((arg = getopt_long_only(argc, argv, shortopts, long_options, &option_index)) != EOF) {
-  //  switch (arg) {
-  //    
-  //  }
-  // }
+  // uint16* port_list = NULL;
+  int option_index = 0;
+  int arg = 0;
+
+  struct option long_options[] = {
+    {"help", no_argument, NULL, 'h'},
+    {"version", no_argument, NULL, 'v'},
+    {0, 0, 0, 0}
+  };
+
+  while ((arg = getopt_long_only(argc, argv, "hv", long_options, &option_index)) != EOF) {
+    switch (arg) {
+
+      case '?':
+      print_usage();
+      exit(1);
+      break;
+
+      case 'h':
+      print_usage();
+      exit(0);
+      break;
+
+      case 'v':
+      print_version();
+      exit(0);
+      break;
+
+      default:
+      fprintf(stderr, "smap -- Unknown argument: %c\n", arg);
+      exit(1);
+    }
+  }
 
   return 0;
 }
 
 void print_version(void) {
-  printf("smap version 0.1 - made by ProfShiba\n");
+  printf("smap version 0.1 -- made by ProfShiba\n");
   exit(0);
 }
 
 void print_usage(void) {
-  printf("smap\nUsage: smap <ip> <flags>\n\n");
-  printf("Example: smap 8.8.8.8 -p 22,80,443,445\n");
+  printf("smap (shiba mapper)\nUsage: smap <FLAGS> <TARGET>\n");
+  printf("\nOPTIONS:\n");
+  printf("-h,  --help    Display this help message\n");
+  printf("-v,  --version    Display the version information\n");
+  printf("\nEXAMPLES:\n");
+  printf("smap --help\n");
+  printf("smap -v\n");
+  // printf("smap -p 22,80,443,445 8.8.8.8\n");
 }
