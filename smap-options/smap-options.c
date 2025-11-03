e966cb1782714f9a95868cb9d1645e75fdff3e1a
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-03 17:39:02 -0600

    add: windows admin check implementation

--- smap-options/smap-options.c
+++ smap-options/smap-options.c
@@ -6,6 +6,25 @@ boolean check_if_root(void) {
     return TRUE;
   return FALSE;
 #else
-  // TODO: windows
+  boolean is_elevated = FALSE;
+	HANDLE hToken = NULL;
+	TOKEN_ELEVATION elevation;
+	DWORD dwSize;
+
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+		goto Cleanup;
+
+	if (!GetTokenInformation(hToken, TokenElevation, &elevation, sizeof(elevation), &dwSize))
+		goto Cleanup;
+
+	is_elevated = elevation.TokenIsElevated;
+
+Cleanup:
+	if (hToken)
+	{
+		CloseHandle(hToken);
+		hToken = NULL;
+	}
+	return is_elevated; 
 #endif
 }
