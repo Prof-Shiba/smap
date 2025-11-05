440938df0c182247d3aff617da11c1f670dfeb2f
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-05 17:28:03 -0600

    update: main now creates scan info ptr and updated TODO notes

--- main.c
+++ main.c
@@ -4,9 +4,27 @@
 #include "./libs/shiba-threads/shiba-threads.h"
 #include "./arg-parsing/arg-parser.h"
 
+// TODO: 1. -st TCP-Connect Scanning
+// 2. -ss SYN scanning
+// 2. -sf FIN scanning
+// 2. -su UDP scanning
+// 2. -sn ping scanning
+
 int main(int argc, char *argv[]) {
-  if (parse_args(argc, argv) != 0) {
+  scan_info_t* s = calloc(1, sizeof(*s));
+  if (!s) {
+    fprintf(stderr, "Failed to allocate space for scan information!\n");
+    return 1;
+  }
+
+  if (parse_args(argc, argv, s) != 0) {
     fprintf(stderr, "Failed to parse args!\n");
     return 1;
   }
+
+  s->address = argv[argc - 1]; // we will validate this later, it might be some garbage input or none at all
+
+  // TODO: Call network handling functions. Will validate address, scan ports, etc
+
+  free(s);
 }
