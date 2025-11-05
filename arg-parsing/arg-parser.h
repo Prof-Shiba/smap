ced47c88d089564b820b6b019a12c52243c726c8
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-05 17:27:28 -0600

    add: comments explaining header and add scan info ptr to parse_args declaration

--- arg-parsing/arg-parser.h
+++ arg-parsing/arg-parser.h
@@ -1,5 +1,10 @@
 #pragma once
 
-int parse_args(int argc, char* argv[]);
+// This contains argument parsing logic as well as the
+// print_usage and version functions, which are only used here
+
+#include "../smap-options/smap-options.h"
+
+int parse_args(int argc, char* argv[], scan_info_t* s);
 void print_usage(char* argv[]);
 void print_version(char* argv[]);
