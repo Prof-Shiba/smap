97866070f25a11ccbf81d88d1be6ef09ca78ee8f
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-06 18:20:08 -0600

    add: FIXME and TODO notes, as well as init_scan_info and malloc for s

--- main.c
+++ main.c
@@ -11,20 +11,22 @@
 // 2. -sn ping scanning
 
 int main(int argc, char *argv[]) {
-  scan_info_t* s = calloc(1, sizeof(*s));
+  scan_info_t* s = malloc(sizeof(*s));
   if (!s) {
     fprintf(stderr, "Failed to allocate space for scan information!\n");
     return 1;
   }
+  init_scan_info(s);
 
   if (parse_args(argc, argv, s) != 0) {
     fprintf(stderr, "Failed to parse args!\n");
     return 1;
   }
 
-  s->address = argv[argc - 1]; // we will validate this later, it might be some garbage input or none at all
+  s->targets = argv[argc - 1]; // FIXME: what if we want multiple IP addresses? Let's just assume all extra input are addresses
 
   // TODO: Call network handling functions. Will validate address, scan ports, etc
 
+  // TODO: one cleanup function that handles all freeing?
   free(s);
 }
