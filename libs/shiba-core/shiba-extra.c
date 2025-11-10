6120c57ecf547a70d98b86a1c2a4e36949bcd703
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-10 14:33:30 -0600

    implement: shiba_fatal variadic function

--- libs/shiba-core/shiba-extra.c
+++ libs/shiba-core/shiba-extra.c
@@ -6,8 +6,8 @@ uint32 shiba_random_int(uint32 num) {
 }
 
 fint32 shiba_fast_inv_square_root(fint32 number) {
-  int64 i;
-	fint32 x2, y;
+  int64 i = 0;
+	fint32 x2 = 0, y = 0;
 	const fint32 threehalfs = 1.5F;
 
 	x2 = number * 0.5F;
@@ -18,4 +18,17 @@ fint32 shiba_fast_inv_square_root(fint32 number) {
 	y  = y * ( threehalfs - ( x2 * y * y ) );
 
 	return y;
+}
+
+void shiba_fatal(const char* message, ...) {
+	va_list args;
+	fflush(stdout);
+	fflush(stderr);
+
+	va_start(args, message);
+	vfprintf(stderr, message, args);
+	va_end(args);
+
+	fprintf(stderr, "\n");
+	exit(1);
 }
