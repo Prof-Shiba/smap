a92a57f42d4c6944818104c41c9629a96622a7b0
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-09 23:17:28 -0600

    implement: unfinished port parsing function with FIXME note

--- port-engine/port-list.c
+++ port-engine/port-list.c
@@ -2,15 +2,10 @@
 
 // NOTE: Formatted as 21,22,445 etc
 int get_ports(char* ports, scan_info_t* s) {
-  // How should we handle this? Take every char until a comma, and make it one uint16? total_ports++
-  // Add to an array? Then getting the s->num_ports etc is easy
-
-  // while port_list != '\0', if char != ',' then add num to string, keep doing until
-  // we hit a comma, and set our index in the array back to 0, then make the array char an int and store
-  // in a 65535 array of ints?
-
-  uint16 arr_index = 0, total_ports = 0;
-  char* temp = "";
+  uint16 arr_index = 0;
+  int64 current_port = 0;
+  char temp[8] = "";
+  char* endptr;
 
   // special case, '-p-' means scan all ports
   if (strcmp(ports, "-") == 0) {
@@ -19,6 +14,29 @@ int get_ports(char* ports, scan_info_t* s) {
 
     s->num_ports = MAX_PORT;
     return 0;
+  }
+  else {
+    for (int i = 0; i <= strlen(ports); i++) {
+      if (ports[i] != ',' && ports[i] != '\0') {
+        temp[arr_index++] = ports[i];
+      }
+      else {
+        current_port = strtol(temp, &endptr, 10);
+
+        if (endptr == ports || *endptr != '\0') { // FIXME: Prevent integer overflows/underflows
+          fprintf(stderr, "Invalid characters (%s) passed to -p!\n", endptr);
+          exit(1);
+        }
+        else if (current_port <= 0 || current_port > MAX_PORT) {
+          fprintf(stderr, "Invalid port (%lld) passed to -p! Ports must be between 1-65535!\n", current_port);
+          exit(1);
+        }
+        s->num_ports++;
+        s->port_list[current_port] = TRUE;
+        memset(temp, 0, sizeof(temp));
+        arr_index = 0;
+      }
+    }
   }
 
   return 0;
