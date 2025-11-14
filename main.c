3f408f1d472b9a1c08fdc3010de3f3aa3dd3ada2
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-14 17:33:49 -0600

    remove unneeded testing functionality

--- main.c
+++ main.c
@@ -4,33 +4,17 @@
 #include "./libs/shiba-threads/shiba-threads.h"
 #include "./arg-parsing/arg-parser.h"
 
-// TODO: 1. -st TCP-Connect Scanning
-// 2. -ss SYN scanning
-// 2. -sf FIN scanning
-// 2. -su UDP scanning
-// 2. -sn ping scanning
-
 int main(int argc, char *argv[]) {
   scan_info_t* s = malloc(sizeof(*s));
-  if (!s) shiba_fatal("Failed to create scan info!");
+  if (!s) shiba_fatal("FATAL: Failed to allocate space for scan info! (main)");
   init_scan_info(s);
 
   // TODO: Get current time, show how long it took for program to finish
 
   if (parse_args(argc, argv, s) != 0)
-    shiba_fatal("Failed to parse args!");
-
-  for (int i = 1; i <= MAX_PORT; i++) {
-    if (s->port_list[i] == TRUE)
-      printf("Port %d is marked for scanning!\n", i);
-    else
-      s->ignored_ports++;
-  }
-  printf("%d ports ignored for scanning!\n", s->ignored_ports);
-  printf("%d ports being scanned!\n", s->num_ports);
+    shiba_fatal("FATAL: Failed to parse args! (main)");
 
   // TODO: Call network handling functions. Will validate address, scan ports, etc
 
-  // TODO: one cleanup function that handles all freeing?
-  free(s);
+  scan_info_cleanup(s);
 }
