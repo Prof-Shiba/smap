a263db543a219dae3bac90bfdfe9cd8419cc6d0a
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-14 19:19:52 -0600

    move: validation functionality to shiba-networking

--- scan-engine/scan-engine.h
+++ scan-engine/scan-engine.h
@@ -4,9 +4,4 @@
 #include <string.h>
 
 // These functions will handle collecting responses and doing the general
-// scanning portion needed for smap, as well as IP address validation,
-// including the type of ip address passed in
-
-/* validates an ip, whether v4 or v6. returns 4 on IPv4 success,
- * 6 on IPv6 success, or 1 if invalid */
-extern int is_valid_ip(const char* target);
+// networking business needed to collect information about the ports
