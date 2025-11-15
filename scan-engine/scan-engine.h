0335768f89c8c1659c7a5cdb74a46453419a54ff
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-14 19:07:34 -0600

    add: declaration for is_valid_ip and notes for it

--- scan-engine/scan-engine.h
+++ scan-engine/scan-engine.h
@@ -1,4 +1,12 @@
 #pragma once
+#include "../libs/shiba-core/shiba.h"
+#include "../libs/shiba-network/shiba-network.h"
+#include <string.h>
 
 // These functions will handle collecting responses and doing the general
-// scanning portion needed for smap
+// scanning portion needed for smap, as well as IP address validation,
+// including the type of ip address passed in
+
+/* validates an ip, whether v4 or v6. returns 4 on IPv4 success,
+ * 6 on IPv6 success, or 1 if invalid */
+extern int is_valid_ip(const char* target);
