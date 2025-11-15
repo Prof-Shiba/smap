a8e21a0de43d99884bfc2cb040b45565ecb14ebb
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-14 19:20:49 -0600

    update: main tests

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "./libs/shiba-network/shiba-network.h"
 #include "./libs/shiba-threads/shiba-threads.h"
 #include "./arg-parsing/arg-parser.h"
+#include "scan-engine/scan-engine.h"
 
 int main(int argc, char *argv[]) {
   scan_info_t* s = malloc(sizeof(*s));
@@ -15,6 +16,16 @@ int main(int argc, char *argv[]) {
     shiba_fatal("FATAL: Failed to parse args! (main)");
 
   // TODO: Call network handling functions. Will validate address, scan ports, etc
+  while (s->targets) {
+    if (shiba_network_is_valid_ip_address(s->targets->target) == 4)
+      printf("IP: %s is a valid IPv4 Address!\n", s->targets->target);
+    else if (shiba_network_is_valid_ip_address(s->targets->target) == 6)
+      printf("IP: %s is a valid IPv6 Address!\n", s->targets->target);
+    else
+      printf("%s is an INVALID ip address!\n", s->targets->target);
+
+    s->targets = s->targets->next;
+  }
 
   scan_info_cleanup(s);
 }
