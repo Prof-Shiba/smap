bcf737db91559431106585f2a154813705990911
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-14 19:07:54 -0600

    implement: unfinished is_valid_ip function. Windows TODO

--- scan-engine/scan-engine.c
+++ scan-engine/scan-engine.c
@@ -6,4 +6,16 @@
 // 2. -su UDP scanning
 // 2. -sn ping scanning
 
+int is_valid_ip(const char* target) {
+#ifdef _WIN32
+#else
+  char buf[INET6_ADDRSTRLEN];
 
+  if (inet_pton(AF_INET, target, buf))
+    return 4;
+  else if (inet_pton(AF_INET6, target, buf))
+    return 6;
+
+#endif
+  return 1;
+}
