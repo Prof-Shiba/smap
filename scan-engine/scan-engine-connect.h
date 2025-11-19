95a7a2412fbecbc99e1ac775bb1b9e4f40f94b79
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-19 17:13:42 -0600

    add: unimplemented scan engine connect files with comments

---
+++ scan-engine/scan-engine-connect.h
@@ -0,0 +1,7 @@
+#include "../libs/shiba-network/shiba-network.h"
+#include "../smap-options/smap-options.h"
+
+// This file will contain functions for the TCP connect
+// portion of scanning, as we don't need raw sockets for it
+
+
