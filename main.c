4b52116539b0cec1e472814222cb2d2457d692e4
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-20 15:23:03 -0600

    add: new notes for functions

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "./libs/shiba-network/shiba-network.h"
 #include "./libs/shiba-threads/shiba-threads.h"
 #include "./arg-parsing/arg-parser.h"
+#include "scan-engine/scan-engine.h"
 
 int main(int argc, char *argv[]) {
   scan_info_t* s = malloc(sizeof(*s));
@@ -14,7 +15,10 @@ int main(int argc, char *argv[]) {
   if (parse_args(argc, argv, s) != 0)
     shiba_fatal("FATAL: Failed to parse args! (main)");
 
-  // TODO: Call network handling functions. Will validate address, scan ports, etc
+  // Im thinking, we have a general scan_ports function
+  // which will check the scan type, and automatically call
+  // the correct function to create a socket for it, then
+  // calls an individual scan_port function in a loop?
 
   scan_info_cleanup(s);
 }
