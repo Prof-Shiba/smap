fa0e2bc572b9da56086e467b90f58b8989314b26
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-20 16:26:00 -0600

    add: unfinished scan_port and scan_ports functions

--- scan-engine/scan-engine.c
+++ scan-engine/scan-engine.c
@@ -1,7 +1,40 @@
 #include "./scan-engine.h"
 
-// TODO: 1. -st TCP-Connect Scanning
-// 2. -ss SYN scanning
+// TODO: 
+// 1. -ss SYN scanning
 // 2. -sf FIN scanning
-// 2. -su UDP scanning
-// 2. -sn ping scanning
+// 3. -su UDP scanning
+// 4. -sn ping scanning
+
+int scan_port(const scan_info_t* s, const uint16 port) {
+  // we need to craft packets and get a socket
+  // ready for each scan type
+  switch(s->scan_type) {
+    case SCAN_TCP:
+      // TODO:
+    break;
+
+    case SCAN_SYN:
+      // TODO:
+    break;
+
+    case SCAN_UDP:
+      // TODO:
+    break;
+
+    case SCAN_FIN:
+      // TODO:
+    break;
+
+    case SCAN_PING:
+      // TODO:
+    break;
+
+    default:
+    shiba_fatal("FATAL: Unknown scan type somehow passed to scan_ports in scan-engine.c!");
+  }
+}
+
+void scan_ports(const scan_info_t* s) {
+    // TODO: Loop through all ports and call scan_port
+}
--- scan-engine/scan-engine.h
+++ scan-engine/scan-engine.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "../libs/shiba-core/shiba.h"
 #include "../libs/shiba-network/shiba-network.h"
-#include <string.h>
+#include "../smap-options/smap-options.h"
 
 // These functions will handle collecting responses and doing the general
 // networking business needed to collect information about the ports
+
+// scan all ports marked by s->port_list
+void scan_ports(const scan_info_t* s);
+
+// scan an individual port by calling the appropriate function
+// for the scan type specified
+int scan_port(const scan_info_t* s, const uint16 port);
