d8062be9d32585cb3697ba5efb511a15b92c247a
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-21 22:33:31 -0600

    add: declarations for scan port functions

--- scan-engine/scan-engine.h
+++ scan-engine/scan-engine.h
@@ -2,13 +2,14 @@
 #include "../libs/shiba-core/shiba.h"
 #include "../libs/shiba-network/shiba-network.h"
 #include "../smap-options/smap-options.h"
+#include "./scan-engine-connect.h"
 
 // These functions will handle collecting responses and doing the general
 // networking business needed to collect information about the ports
 
-// scan all ports marked by s->port_list
-void scan_ports(const scan_info_t* s);
+// scan all hosts and their ports marked by s->port_list
+extern void scan_ports(scan_info_t* s);
 
 // scan an individual port by calling the appropriate function
 // for the scan type specified
-int scan_port(const scan_info_t* s, const uint16 port);
+extern int scan_port(const scan_info_t* s, const uint16 port);
