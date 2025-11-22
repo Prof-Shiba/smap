9dd693c73b3f888c2c86e51bc3c3a0fa3d3e4ccf
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-11-21 22:34:37 -0600

    add: open tcp connect declaration

--- scan-engine/scan-engine-connect.h
+++ scan-engine/scan-engine-connect.h
@@ -4,4 +4,5 @@
 // This file will contain functions for the TCP connect
 // portion of scanning, as we don't need raw sockets for it
 
-
+// Open a normal TCP connection. Returns 0 on success, 1 on failure.
+extern int open_tcp_connect(scan_info_t* s, const uint16 port);
