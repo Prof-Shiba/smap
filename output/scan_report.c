ddbeb90fe4a4f7efd37d01367f4958f133aa31fc
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-12-05 21:43:31 -0600

    add: new scan report function

--- output/scan_report.c
+++ output/scan_report.c
@@ -1,28 +1,45 @@
 #include "./scan_report.h"
 
-// NOTE: The print report function will change as we optimize
-// how the scan_info_t type looks and how we handle the port_list
-
 // FIXME: WE ARE ONLY ASSUMING 1 IP ADDR FOR NOW. FIX LATER
-// WE ALSO NEED TO TRACK WHICH PORTS WERE ACTUALLY SCANNED
-// PORT LIST MUST BE VASTLY REWORKED ASAP
 void print_report(const scan_info_t* s) {
-  // We need to go through every target ptr, show which ports were open
-  // alongside with scan type was used, (22/tcp), ignored ports, closed ports
-  // num of scanned ports
   fflush(stdout);
-  printf("==================================================\n");
+  printf("Starting smap scan for %s\n", s->targets->target);
   printf("smap scan results for: %s\n", s->targets->target);
-  printf("Scanned: %d port(s)\n", s->num_ports);
-  printf("Ignored: %d port(s)\n", s->ignored_ports);
+  printf("Scanned: %d port(s)\t", s->num_ports_to_scan);
+  printf("Ignored: %d port(s)\t", s->ignored_ports);
+  printf("Found %d open port(s)\t", s->open_ports);
   printf("%d closed port(s)\n", s->closed_ports);
-  printf("==================================================\n\n");
-  printf("PORT:\n");
+  printf("PORT:\tSTATE:\n");
+  
+  for (int i = 0; i < s->num_ports_to_scan; i++) {
+    if (s->port_list[s->port_nums[i]].state == PORT_OPEN) {
+      printf("%d\tOPEN", s->port_nums[i]);
 
-  for (int i = 1; i <= MAX_PORT; i++) {
-    if (s->port_list[i] == TRUE) {
-      printf("%d\tOPEN\n", i);
+      switch(s->scan_type) {
+        case SCAN_TCP:
+          printf("/TCP\n");
+          break;
+
+        case SCAN_SYN:
+          printf("/SYN\n");
+          break;
+
+        case SCAN_UDP:
+          printf("/UDP\n");
+          break;
+
+        case SCAN_FIN:
+          printf("/FIN\n");
+          break;
+
+        case SCAN_PING:
+          printf("/PING\n");
+          break;
+
+        default:
+          printf("(DEBUG ME) Reached end case of switch statement in: %s", __FILE__);
+          break;
+      }
     }
   }
-
 }
