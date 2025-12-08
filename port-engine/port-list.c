9cedd667438b9d7766a194f1578f1cfd5cae1d20
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-12-08 15:01:06 -0600

    add: dynamically resizing port nums array logic for many ports manually entered

--- port-engine/port-list.c
+++ port-engine/port-list.c
@@ -2,10 +2,9 @@
 
 // FIXME: If i manually put port 8000, it doesnt find it
 // -p- WILL find it, but it will display random shit too
-// This makes me think it might be 2 problems. Ones a port initialization
-// or buffer overrun issue, the others a actual port scanning function issue
 
 int get_ports(const char* ports, scan_info_t* s) {
+  u16 j = 0;
   u16 arr_index = 0;
   i64 current_port = 0;
   char temp[8] = "";
@@ -17,7 +16,6 @@ int get_ports(const char* ports, scan_info_t* s) {
     if (!s->port_nums)
       shiba_fatal("Realloc failed for port nums! (%s)", __FILE_NAME__);
 
-    // FIXME: This isnt doing anything
     for (int i = 1; i <= MAX_PORT; i++)
       s->port_nums[i] = i;
 
@@ -38,12 +36,28 @@ int get_ports(const char* ports, scan_info_t* s) {
           || current_port <= 0 || current_port > MAX_PORT) {
           shiba_fatal("Invalid port passed to -p! Ports must be an integer between 1-65535");
         }
-        // FIXME: Check if port_nums is too small for the current capacity
         if (s->port_list[current_port].port_num == 0) {
           s->num_ports_to_scan++;
-          s->port_list[current_port].port_num = current_port;
+          s->port_list[current_port].port_num = current_port; // NOTE: storage
+          s->port_nums[j++] = current_port;
+
+          if (j >= s->port_max_size) {
+            s->port_max_size *= 2;
+            // this may be an issue because the extra spots are not zero'd out
+            u32* tmp = realloc(s->port_nums, s->port_max_size * sizeof(*s->port_nums));
+            if (!tmp)
+              shiba_fatal("FATAL: Realloc failed on port nums! (%s)", __FILE_NAME__);
+
+            // NOTE: Debug this to verify it does whats intended
+            for (int i = s->port_max_size / 2; i < s->port_max_size; i++) {
+              tmp[i] = 0;
+            }
+
+            s->port_nums = tmp;
+          }
         }
         else {
+          // NOTE: might be better to have a dupe flag or something so we dont spam messages for the same port being duped.
           printf("You've entered that port (%ld) already! Pay attention, you might bring a whole network down!\n", current_port);
         }
         memset(temp, 0, sizeof(temp));
