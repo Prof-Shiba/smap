1bbd57121a1bb94bd6c196ed272d3ab2d0154cf7
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-12-13 18:03:55 -0600

    add: move var declarations to else statement and fix some loop OOB issues. not entirely fixed yet for -p-

--- port-engine/port-list.c
+++ port-engine/port-list.c
@@ -3,30 +3,35 @@
 // FIXME: -p- displays random shit in unpredictable amounts
 
 int get_ports(const char* ports, scan_info_t* s) {
-  u16 j = 0;
-  u16 arr_index = 0;
-  i64 current_port = 0;
-  char temp[8] = "";
-  char* endptr;
-
   // special case, '-p-' means scan all ports
   if (strcmp(ports, "-") == 0) {
     s->port_max_size = MAX_PORT;
+    s->num_ports_to_scan = MAX_PORT;
 
-    u32* tmp = realloc(s->port_nums, sizeof(*s->port_nums) * (s->port_max_size + 1));
+    u32* tmp = realloc(s->port_nums, MAX_PORT * sizeof(*s->port_nums));
     if (!tmp)
       shiba_fatal("Realloc failed for port nums! (%s)", __FILE_NAME__);
 
+    memset(tmp, 0, MAX_PORT * sizeof(*s->port_nums));
+
     s->port_nums = tmp;
 
-    for (int i = 1; i <= MAX_PORT; i++)
-      s->port_nums[i] = i;
+    for (int i = 1; i <= MAX_PORT; i++) {
+      s->port_list[i].port_num = i;
+      s->port_list[i].state = PORT_UNKNOWN;
+      s->port_nums[i - 1] = i;
+    }
 
-    s->num_ports_to_scan = MAX_PORT;
     return 0;
   }
   // TODO: add check here later for a -, indicating a range of ports to scan
   else {
+    u16 j = 0;
+    u16 arr_index = 0;
+    i64 current_port = 0;
+    char temp[8] = "";
+    char* endptr;
+
     for (int i = 0; i <= strlen(ports); i++) {
       if (ports[i] != ',' && ports[i] != '\0') {
         if (arr_index >= 6)
@@ -42,7 +47,7 @@ int get_ports(const char* ports, scan_info_t* s) {
         }
         if (s->port_list[current_port].port_num == 0) {
           s->num_ports_to_scan++;
-          s->port_list[current_port].port_num = current_port; // NOTE: storage
+          s->port_list[current_port].port_num = current_port;
           s->port_nums[j++] = current_port;
 
           if (j >= s->port_max_size) {
@@ -55,13 +60,14 @@ int get_ports(const char* ports, scan_info_t* s) {
             // NOTE: Debug this to verify it does whats intended
             for (int i = s->port_max_size / 2; i < s->port_max_size; i++) {
               tmp[i] = 0;
+              s->port_list[i].port_num = 0;
+              s->port_list[i].state = PORT_UNKNOWN;
             }
 
             s->port_nums = tmp;
           }
         }
         else {
-          // NOTE: might be better to have a dupe flag or something so we dont spam messages for the same port being duped.
           printf("You've entered that port (%ld) already! Pay attention, you might bring a whole network down!\n", current_port);
         }
         memset(temp, 0, sizeof(temp));
