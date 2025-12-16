e3c574e92c49a3857dd4656f0d5994cddc7d2510
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2025-12-15 21:39:16 -0600

    fix: detect AF type before creating socket

--- scan-engine/scan-engine-connect.c
+++ scan-engine/scan-engine-connect.c
@@ -3,16 +3,16 @@
 int open_tcp_connect(scan_info_t* s, const u16 port) {
   int ret_val = 1;
 
-  shiba_network_socket_t* socket = shiba_network_create_socket(s->af, s->sock_type, 0);
-  if (!socket)
-    return ret_val;
-
-  struct sockaddr_storage addr;
-
   if (shiba_network_return_ip_type(s->targets->target) == 4)
     s->af = AF_INET;
   else
     s->af = AF_INET6;
+
+  shiba_network_socket_t* socket = shiba_network_create_socket(s->af, s->sock_type, 0);
+  if (!socket)
+    return ret_val;
+
+  struct sockaddr_storage addr = {0};
 
   if (s->af == AF_INET) {
     struct sockaddr_in* new_addr = (struct sockaddr_in*)&addr;
