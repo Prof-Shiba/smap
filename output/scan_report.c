ccadda186792437b69b13f45976b2e040ddf2547
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-01-10 14:29:54 -0600

    remove: unneeded comment. function works on linux

--- output/scan_report.c
+++ output/scan_report.c
@@ -11,10 +11,6 @@ void print_report(const scan_info_t* s) {
   printf("%d closed port(s)\n", s->closed_ports);
   printf("PORT:\tSTATE:\n");
  
-  // TODO: Check on linux if -p- scans still hit 65535
-  // i removed the <= and made it < because on windows
-  // i hit an OOB access violation on smaller scans.
-  // Look into it!
   for (int i = 0; i < s->num_ports_to_scan; i++) {
     if (s->port_list[s->port_nums[i]].state == PORT_OPEN) {
       printf("%d\tOPEN", s->port_nums[i]);
