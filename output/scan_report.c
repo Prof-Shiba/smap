175ffa8a120c3f97407aeb9d32f0e132d8e25695
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-02-03 12:09:37 -0600

    refactor: scan report output to accomodate multiple ip addresses

--- output/scan_report.c
+++ output/scan_report.c
@@ -1,46 +1,50 @@
 #include "./scan_report.h"
 
-// FIXME: WE ARE ONLY ASSUMING 1 IP ADDR FOR NOW. FIX LATER
 // FIXME: Print them in order of smallest to largest port
-void print_report(const scan_info_t* s, const f32 cpu_time) {
+void print_report(scan_info_t* s, const f32 cpu_time) {
   fflush(stdout);
-  printf("smap scan results for: %s\n", s->targets->target);
-  printf("Scanned: %d port(s)\t", s->num_ports_to_scan);
-  printf("Ignored: %d port(s)\t", s->ignored_ports);
-  printf("Found %d open port(s)\t", s->open_ports);
-  printf("%d closed port(s)\n", s->closed_ports);
-  printf("PORT:\tSTATE:\n");
- 
-  for (int i = 0; i < s->num_ports_to_scan; i++) {
-    if (s->port_list[s->port_nums[i]].state == PORT_OPEN) {
-      printf("%d\tOPEN", s->port_nums[i]);
 
-      switch(s->scan_type) {
-        case SCAN_TCP:
-          printf("/TCP\n");
-          break;
+  while (s->targets) {
+    printf("\nsmap scan results for: %s\n", s->targets->target);
+    printf("Scanned: %d port(s)\t", s->num_ports_to_scan);
+    printf("Ignored: %d port(s)\t", s->targets->port_list->ignored_ports);
+    printf("Found %d open port(s)\t", s->targets->port_list->open_ports);
+    printf("%d closed port(s)\n", s->targets->port_list->closed_ports);
+    printf("PORT:\tSTATE:\n");
+  
+    for (int i = 0; i < s->num_ports_to_scan; i++) {
+      if (s->targets->port_list[s->port_nums[i]].state == PORT_OPEN) {
+        printf("%d\tOPEN", s->port_nums[i]);
 
-        case SCAN_SYN:
-          printf("/SYN\n");
-          break;
+        switch(s->scan_type) {
+          case SCAN_TCP:
+            printf("/TCP\n");
+            break;
 
-        case SCAN_UDP:
-          printf("/UDP\n");
-          break;
+          case SCAN_SYN:
+            printf("/SYN\n");
+            break;
 
-        case SCAN_FIN:
-          printf("/FIN\n");
-          break;
+          case SCAN_UDP:
+            printf("/UDP\n");
+            break;
 
-        case SCAN_PING:
-          printf("/PING\n");
-          break;
+          case SCAN_FIN:
+            printf("/FIN\n");
+            break;
 
-        default:
-          printf("(DEBUG ME) Reached default case of switch statement in (%s)", __FILE__);
-          break;
+          case SCAN_PING:
+            printf("/PING\n");
+            break;
+
+          default:
+            printf("(DEBUG ME) Reached default case of switch statement in (%s)", __FILE__);
+            break;
+        }
       }
     }
+
+    s->targets = s->targets->next;
   }
 
   (s->num_targets == 1) ?
--- output/scan_report.h
+++ output/scan_report.h
@@ -1,4 +1,4 @@
 #include "../libs/shiba-core/shiba.h"
 #include "../smap-options/smap-options.h"
 
-void print_report(const scan_info_t* s, const f32 cpu_time);
+void print_report(scan_info_t* s, const f32 cpu_time);
