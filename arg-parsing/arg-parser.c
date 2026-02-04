53f670ba1e61509dabda3aff76cf7520f83e8169
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-02-03 22:22:35 -0600

    update: print usage and add call to html create function

--- arg-parsing/arg-parser.c
+++ arg-parsing/arg-parser.c
@@ -28,13 +28,13 @@ int parse_args(int argc, char *argv[], scan_info_t* s) {
     {"timeout", NULL, required_argument, 't'},
 
     //output related fields (file format etc)
-    {"output", NULL, optional_argument, 'o'},
+    {"output", NULL, required_argument, 'o'},
 
     // NULL TERM
     {0, 0, 0, 0}
   };
 
-  while ((arg = getopt_long_only(argc, argv, "hvp:s:t:o::", long_options, &option_index)) != EOF) {
+  while ((arg = getopt_long_only(argc, argv, "hvp:s:t:o:", long_options, &option_index)) != EOF) {
     switch (arg) {
 
       case '?':
@@ -93,10 +93,10 @@ int parse_args(int argc, char *argv[], scan_info_t* s) {
 
       case 'o':
         if (strcmp(opt_arg, "H") == 0) {
-          // we need to create the html stuff
+          create_html_template(++opt_arg); // TODO: test
         }
         else {
-          shiba_fatal("Unknown output argument: %s", opt_arg);
+          shiba_fatal("Unknown output argument: %s. Try smap --help", opt_arg);
         }
       break;
 
@@ -110,7 +110,7 @@ int parse_args(int argc, char *argv[], scan_info_t* s) {
     link_ips(argc, argv, s);
   }
   else {
-    shiba_fatal("FATAL: Target address not specified! Try smap -h");
+    shiba_fatal("FATAL: Target address not specified! Try smap --help");
   }
 
   return 0;
@@ -122,21 +122,28 @@ void print_version(char* argv[]) {
 
 void print_usage(char* argv[]) {
   printf("smap (shiba mapper). A simple port scanner.\nUsage: %s <FLAGS> <TARGET>\n", argv[0]);
+
   printf("\nOPTIONS:\n");
+  printf("Misc:\n");
   printf("-h,  --help       Display this help message\n");
   printf("-v,  --version    Display the version information\n");
+
+  printf("Scan options:\n");
   printf("-t,  --timeout    Set the timeout in milliseconds for scans\n");
   printf("-p,  --port       Set the port(s) to be scanned\n");
   printf("-s,               Set the scan type (-sT, -sS, -sU) *note: sS and sU not yet implemented*\n");
-  printf("-o,  --output     Output scan results to a file. Takes the file type as a parameter, and an optional parameter, which is the name\n");
-  printf("-oH,              Output scan results to a HTML file.\n");
+
+  printf("Output Options:\n");
+  printf("-o,               Output scan results to a file. Takes the file type and file name as parameters. See below options.\n");
+  printf("-oH,              Output scan results to a HTML file. Includes custom smap_css styling by default.\n");
+
   printf("\nEXAMPLES:\n");
   printf("%s --help\n", argv[0]);
   printf("%s -v\n", argv[0]);
   printf("smap -p 22,80,443,445 8.8.8.8\n");
   printf("smap 8.8.8.8 -p-\n");
   printf("smap 127.0.0.1 -sT -p22 --timeout 2000\n");
-  printf("smap -p- 127.0.0.1 172.33.11.195 -oH scan_results");
+  printf("smap -p- 127.0.0.1 172.33.11.195 -oH scan_results\n");
 }
 
 void parse_timeout(const char* timeout, scan_info_t* s) {
