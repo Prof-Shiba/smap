ede5ad95e309390c10215c918690b3c2bfe3c8bb
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-02-05 16:52:37 -0600

    add: print and handle report functions

--- output/scan_report.h
+++ output/scan_report.h
@@ -1,4 +1,10 @@
 #include "../libs/shiba-core/shiba.h"
 #include "../smap-options/smap-options.h"
+#include "../output/print_time.h"
+#include "./html.h"
 
-void print_report(scan_info_t* s, const f32 cpu_time);
+void handle_report(scan_info_t* s, const f32 cpu_time);
+void print_report(scan_info_t* s, FILE* f, const f32 cpu_time);
+void print_html_report(scan_info_t* s, FILE* f, const f32 cpu_time);
+void print_greppable_report(scan_info_t* s, FILE* f, const f32 cpu_time);
+void print_smap_file_report(scan_info_t* s, FILE* f, const f32 cpu_time);
