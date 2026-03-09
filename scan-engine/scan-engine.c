81863d2d69294386f00254d9a4af1d80149cc799
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-03-09 18:57:47 -0500

    remove: comments and setting is_host_alive for testing

--- scan-engine/scan-engine.c
+++ scan-engine/scan-engine.c
@@ -1,23 +1,12 @@
 #include "./scan-engine.h"
+#include "host-alive.h"
 
 void scan_ports(scan_info_t* s) {
   target_t* head = s->targets;
 
   while (s->targets) {
-    // NOTE: Temp for testing. We need to ping host, verify its alive,
-    // and then that functions return res will determine if we set
-    // to alive or not (not handled here)
-    s->targets->is_host_alive = TRUE;
-
     if (s->targets->is_host_alive == TRUE) {
       for (int i = 0; i < s->num_ports_to_scan; i++) {
-        // NOTE: i think its less work if the function called by scan_port 
-        // checks if the host is alive and returns -1 if not, right? but if
-        // we do that, then it limits our options for determing if the host is up
-        // eg. maybe a ICMP packet gets dropped due to firewall, but trying from
-        // port 53 works and we can determine the servers up. so i think
-        // i should make a separate function which will independently determine if the
-        // host is alive/dead, then we set targets->is_host_alive based on that
         if (scan_port(s, s->port_nums[i]) == 0) {
           s->targets->port_list[s->port_nums[i]].state = PORT_OPEN;
           s->targets->open_ports++;
