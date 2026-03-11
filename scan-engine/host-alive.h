7fd38d788addaef37331959f3de3e0c5d90e4e72
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-03-11 14:09:50 -0500

    add: TODO Note

--- scan-engine/host-alive.h
+++ scan-engine/host-alive.h
@@ -4,8 +4,13 @@
   a full port scan so we can skip wasting time on dead/non-existent hosts
 */
 
+// TODO: heres what we can do:
+// non root-users can use a TCP connect to port 80 or 443 to see if host is alive
+// root users have more options. ICMP echo requests, ACK to port 80 for example,
+// SYN to port 443, an ICMP timestamp request, udp, and more. We will focus on implementing these.
+
 #pragma once
 
 #include "./scan-engine.h"
 
-
+// https://www.rfc-editor.org/rfc/rfc1071 ICMP checksum here
