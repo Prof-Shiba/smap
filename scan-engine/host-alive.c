01f6fb10b48b235dddf10f8408bc6727c792f125
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-05-07 16:46:55 -0500

    finish icmp4 ping implementation with TODO note

--- scan-engine/host-alive.c
+++ scan-engine/host-alive.c
@@ -12,11 +12,11 @@ u16 icmp_checksum(u16* data, size_t size) {
   return ~((sum << 16 >> 16) + (sum >> 16));
 }
 
-void icmp4_ping_request(void) {
+boolean icmp4_ping_request(scan_info_t* s) {
   shiba_network_socket_t* sock = shiba_network_create_socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
   if (!sock) {
     fprintf(stderr, "Failed to create shiba ICMP socket in %s on line %d!\n", __FILE__, __LINE__);
-    return;
+    return FALSE;
   }
 
   s_icmp_hdr icmp = {
@@ -27,8 +27,36 @@ void icmp4_ping_request(void) {
     .sequence = 0
   };
   icmp.checksum = icmp_checksum((u16*)&icmp, sizeof(icmp));
-  
+
+  struct sockaddr_in dst_addr = {0};
+  dst_addr.sin_family = s->af;
+  dst_addr.sin_addr.s_addr = inet_addr(s->targets->target);
+
+  struct timeval tv = {0};
+  tv.tv_sec = 0;
+  tv.tv_usec = s->timeout_ms;
+  setsockopt(sock->handle, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof(tv));
+
+  int res = sendto(sock->handle, &icmp, sizeof(icmp), 0, (struct sockaddr*)&dst_addr, sizeof(dst_addr));
+  if (res < 0) {
+    fprintf(stderr, "sendto() failed in %s!\n", __FILE__);
+    return FALSE;
+  }
+
+  printf("Sent %d bytes to %s!\n", res, s->targets->target);
+  struct sockaddr_in client_addr = {0};
+  socklen_t client_len = sizeof(client_addr);
+
+  res = recvfrom(sock->handle, &icmp, sizeof(icmp), 0, (struct sockaddr*)&client_addr, &client_len);
+  // TODO: Make this a verbose option later. But useful for now imo.
+  if (res == -1) {
+    fprintf(stderr, "Failed to receive any bytes from %s. Host is likely down.\n", s->targets->target);
+    return FALSE;
+  }
+
+  printf("Received response from host %s!\n", s->targets->target);
 
   shiba_network_destroy_socket(sock);
+  return TRUE;
 }
 
