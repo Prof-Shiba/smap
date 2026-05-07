ec983428557142f5b0519e44438b8c9133a1e8f8
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-05-06 21:23:37 -0500

    add: icmp checksum implementation, unfinished icmp4 ping req

--- scan-engine/host-alive.c
+++ scan-engine/host-alive.c
@@ -1,3 +1,34 @@
 #include "./host-alive.h"
 
+u16 icmp_checksum(u16* data, size_t size) {
+  u32 sum = 0;
+  size_t data_len = size / 2;
+
+  for (size_t i = 0; i < data_len; i++) {
+    sum += data[i];
+  }
+
+  // fold 32 bits to 16
+  return ~((sum << 16 >> 16) + (sum >> 16));
+}
+
+void icmp4_ping_request(void) {
+  shiba_network_socket_t* sock = shiba_network_create_socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+  if (!sock) {
+    fprintf(stderr, "Failed to create shiba ICMP socket in %s on line %d!\n", __FILE__, __LINE__);
+    return;
+  }
+
+  s_icmp_hdr icmp = {
+    .type = ICMP_ECHO,
+    .code = 0,
+    .checksum = 0,
+    .id = 0,
+    .sequence = 0
+  };
+  icmp.checksum = icmp_checksum((u16*)&icmp, sizeof(icmp));
+  
+
+  shiba_network_destroy_socket(sock);
+}
 
