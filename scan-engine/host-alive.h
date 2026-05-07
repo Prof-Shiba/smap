75dbb543031020ebc1b4644d5eac73cec081e006
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-05-07 17:12:48 -0500

    refactor icmp4 sig, add verify host status

--- scan-engine/host-alive.h
+++ scan-engine/host-alive.h
@@ -4,12 +4,6 @@
   a full port scan so we can skip wasting time on dead/non-existent hosts
 */
 
-// TODO: heres what we can do:
-// non root-users can use a TCP connect to port 80 or 443 to see if host is alive
-// root users have more options. ICMP echo requests, ACK to port 80 for example,
-// SYN to port 443, an ICMP timestamp request, udp, and more. I will focus on ICMP and TCP
-// then come back to these other options later after more scanning methods are added.
-
 #pragma once
 
 #define ICMP_ECHOREPLY 0	
@@ -38,9 +32,9 @@ typedef struct {
   u16 sequence;
 } __attribute__((packed)) s_icmp_hdr;
 
-// should I have one function that calls other functions, or just have separate functions?
-// i will (for now at least) just make an ICMP ping request function to test. But consider the above...
-boolean icmp4_ping_request(scan_info_t* s);
+boolean icmp4_ping_request(const scan_info_t* s, const char* host);
 
 // https://www.rfc-editor.org/rfc/rfc1071 ICMP checksum here
 u16 icmp_checksum(u16* data, size_t size);
+
+void verify_host_status(scan_info_t* s);
