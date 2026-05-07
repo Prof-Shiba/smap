cae4b3aab091ecbde51816fb82bb71f659340b92
ProfShiba <166961473+Prof-Shiba@users.noreply.github.com>
2026-05-06 21:23:11 -0500

    add: ICMP defines, s_icmp_hdr struct, icmp4 ping and icmp_checksum func

--- scan-engine/host-alive.h
+++ scan-engine/host-alive.h
@@ -1,16 +1,46 @@
 /*
-  host-alive contains functions which will make use of ICMP to probe
-  whether a host is alive or down. This is used prior to initiating
+  host-alive contains functions which will make use of ICMP and various other 
+  methods to probe whether a host is alive or down. This is used prior to initiating
   a full port scan so we can skip wasting time on dead/non-existent hosts
 */
 
 // TODO: heres what we can do:
 // non root-users can use a TCP connect to port 80 or 443 to see if host is alive
 // root users have more options. ICMP echo requests, ACK to port 80 for example,
-// SYN to port 443, an ICMP timestamp request, udp, and more. We will focus on implementing these.
+// SYN to port 443, an ICMP timestamp request, udp, and more. I will focus on ICMP and TCP
+// then come back to these other options later after more scanning methods are added.
 
 #pragma once
 
+#define ICMP_ECHOREPLY 0	
+#define ICMP_DEST_UNREACH	3	
+#define ICMP_ECHO 8
+
+// ICMP UNREACHABLE CODES
+#define ICMP_NET_UNREACH	0
+#define ICMP_HOST_UNREACH	1
+#define ICMP_PROT_UNREACH	2
+#define ICMP_PORT_UNREACH	3
+#define ICMP_FRAG_NEEDED	4
+#define ICMP_SR_FAILED		5
+#define ICMP_NET_UNKNOWN	6
+#define ICMP_HOST_UNKNOWN	7
+#define ICMP_HOST_ISOLATED 8
+
 #include "./scan-engine.h"
 
+// https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol
+typedef struct {
+  u8 type;
+  u8 code;
+  u16 checksum;
+  u16 id;
+  u16 sequence;
+} __attribute__((packed)) s_icmp_hdr;
+
+// should I have one function that calls other functions, or just have separate functions?
+// i will (for now at least) just make an ICMP ping request function to test. But consider the above...
+void icmp4_ping_request(void);
+
 // https://www.rfc-editor.org/rfc/rfc1071 ICMP checksum here
+u16 icmp_checksum(u16* data, size_t size);
